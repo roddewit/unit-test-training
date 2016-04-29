368aee928368ee0c7b1b9304eb9ad02308fe8663
cwquick <collin.quick@varian.com>
2016-04-29 13:59:46 -0500

    Exercise #3 - Add new unit test to fully test the Purchase() function in Store.cs
    ([Ignore] attribute in IntegrationTest has been commented out again for test execution completeness)

--- UnitTestProject/StoreTests.cs
+++ UnitTestProject/StoreTests.cs
@@ -132,6 +132,28 @@ namespace UnitTestProject
         }
 
 
+        [Test]
+        [ExpectedException(typeof(OutOfStockException))]
+        public void Test_ProductOutOfStock()
+        {
+            //Arrange
+            const string TEST_PRODUCT_ID = "1";
+
+            var users = new List<User>();
+            users.Add(createTestUser("Test User", "", 100.00));
+
+            var products = new List<Product>();
+            products.Add(createTestProduct(TEST_PRODUCT_ID, "Product", 5, 10));
+
+            var dataManager = new DataManager(users, products);
+            var store = new Store(users[0], dataManager);
+
+            //Act
+            store.Purchase(TEST_PRODUCT_ID, 12);
+            Assert.Fail("OutOfStockException was not thrown");   // Should not reach this point due to the [ExpectedException] attribute on this test
+        }
+
+
         // THE BELOW CODE IS REQUIRED TO PREVENT THE TESTS FROM MODIFYING THE USERS/PRODUCTS ON FILE
         //  This is not a good unit testing pattern - the unit test dependency on the file system should
         //  actually be broken ... training on how to do this will be coming.
