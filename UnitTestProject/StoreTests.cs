9e7a04af3f8bcfc1d4a4436f2d17f8a90ecd6bc6
kun.su <kunsupython@gmail.com>
2016-04-29 01:38:55 -0500

    experience some c#

--- UnitTestProject/StoreTests.cs
+++ UnitTestProject/StoreTests.cs
@@ -34,21 +34,26 @@ namespace UnitTestProject
             return testProduct;
         }
 
+        private Store arrangeSingleProductSingleUserTest( 
+                                  string productId, string testUserName="Test User", int fund=100, 
+                                  string productName="Product", int price=1, int availableToPurchase=10,
+                                  List<Product> productList = null )
+        {
+            var users = new List<User>();
+            users.Add( createTestUser( testUserName, "", fund ) );
+            var products = (productList==null)? new List<Product>():productList;
+            products.Add( createTestProduct( productId, "Product", price, availableToPurchase ) );
+            var dataManager = new DataManager(users, products);
+            var store = new Store(users[0], dataManager);
+            return store;
+        }
+
         [Test]
         public void Test_PurchaseThrowsNoErrorForValidFunds()
         {
             //Arrange
             const string TEST_PRODUCT_ID = "1";
-
-            var users = new List<User>();
-            users.Add(createTestUser("Test User", "", 99.99));
-
-            var products = new List<Product>();
-            products.Add(createTestProduct(TEST_PRODUCT_ID, "Product", 9.99, 10));
-
-            var dataManager = new DataManager(users, products);
-            var store = new Store(users[0], dataManager);
-
+            var store = arrangeSingleProductSingleUserTest(TEST_PRODUCT_ID);
             //Act
             store.Purchase(TEST_PRODUCT_ID, 10);
 
@@ -61,82 +66,65 @@ namespace UnitTestProject
         {
             //Arrange
             const string TEST_PRODUCT_ID = "1";
-
-            var users = new List<User>();
-            users.Add( createTestUser( "Test User", "", 99.99 ) );
-
             var products = new List<Product>();
-            products.Add( createTestProduct( TEST_PRODUCT_ID, "Product", 9.99, 10 ) );
-
-            var dataManager = new DataManager(users, products);
-            var store = new Store(users[0], dataManager);
+            var store = arrangeSingleProductSingleUserTest(
+                TEST_PRODUCT_ID, 
+                availableToPurchase:10, 
+                productList:products);
 
             //Act
             store.Purchase( TEST_PRODUCT_ID, 9 );
-            //Assert 
-            //(choose the appropriate statement(s))
-            //Assert.AreEqual(1, products[0].Quantity);
-            //Assert.AreSame(1, products[0].Quantity);
+
             Assert.IsTrue(products[0].Quantity == 1);
         }
 
         [Test]
+        [ExpectedException( typeof( InsufficientFundsException ) )]
         public void Test_PurchaseThrowsExceptionWhenBalanceIsTooLow()
         {
             //Arrange
             const string TEST_PRODUCT_ID = "1";
-
-            var users = new List<User>();
-            users.Add( createTestUser( "Test User", "", 9.98 ) );
-
-            var products = new List<Product>();
-            products.Add( createTestProduct( TEST_PRODUCT_ID, "Product", 9.99, 10 ) );
-
-            var dataManager = new DataManager(users, products);
-            var store = new Store(users[0], dataManager);
-
-            //Act
-            try {
-                store.Purchase( TEST_PRODUCT_ID, 1 );
-            //Assert
-                Assert.Fail();
-            }
-            catch ( Exception e)
-            {
-                Assert.IsInstanceOf( typeof(InsufficientFundsException), e);
-            }
-            
+            //act
+            var store = arrangeSingleProductSingleUserTest(
+                TEST_PRODUCT_ID, 
+                fund:1, 
+                price:10);
+            store.Purchase( TEST_PRODUCT_ID, 1 );
         }
 
         [Test]
+        [ExpectedException( typeof( InsufficientFundsException ) )]
         public void Test_PurchaseMultipleItemThrowsExceptionWhenBalanceIsTooLow()
         {
             //Arrange
             const string TEST_PRODUCT_ID = "1";
+            const int QUANTITY_TO_PURCHASE = 11;
+            const int price = 1;
 
-            var users = new List<User>();
-            users.Add( createTestUser( "Test User", "", 9.98 ) );
-
-            var products = new List<Product>();
-            products.Add( createTestProduct( TEST_PRODUCT_ID, "Product", 9, 10 ) );
-
-            var dataManager = new DataManager(users, products);
-            var store = new Store(users[0], dataManager);
+            var store = arrangeSingleProductSingleUserTest(
+                TEST_PRODUCT_ID, price:price, 
+                fund:(QUANTITY_TO_PURCHASE-1)*price, 
+                availableToPurchase:QUANTITY_TO_PURCHASE );
 
             //Act
-            try
-            {
-                store.Purchase( TEST_PRODUCT_ID, 10 );
-                //Assert
-                Assert.Fail();
-            }
-            catch ( Exception e )
-            {
-                Assert.IsInstanceOf( typeof( InsufficientFundsException ), e );
-            }
-
+            store.Purchase( TEST_PRODUCT_ID, QUANTITY_TO_PURCHASE );
         }
 
+        [Test]
+        [ExpectedException( typeof( OutOfStockException ) )]
+        public void Test_PurchaseThrowsExceptionWhenNotEnoughQuantity ()
+        {
+            //Arrange
+            const string TEST_PRODUCT_ID = "1";
+            const int QUANTITY_TO_PURCHASE = 11;
+
+            var store = arrangeSingleProductSingleUserTest(
+                TEST_PRODUCT_ID, 
+                availableToPurchase:QUANTITY_TO_PURCHASE-1);
+
+            //Act
+            store.Purchase( TEST_PRODUCT_ID, QUANTITY_TO_PURCHASE );
+        }
 
         // THE BELOW CODE IS REQUIRED TO PREVENT THE TESTS FROM MODIFYING THE USERS/PRODUCTS ON FILE
         //  This is not a good unit testing pattern - the unit test dependency on the file system should
