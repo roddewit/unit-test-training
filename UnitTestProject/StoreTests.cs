071f7996b40c8ab3a236d8c1cd843eabad8bb48c
dmcconnell37 <david.mcconnell@varian.com>
2016-04-29 14:42:48 -0500

    Exercise #3

--- UnitTestProject/StoreTests.cs
+++ UnitTestProject/StoreTests.cs
@@ -139,6 +139,29 @@ namespace UnitTestProject
         }
 
 
+        [Test]
+        [ExpectedException(typeof(OutOfStockException))]
+        public void Test_PurchaseThrowsExceptionWhenNotEnoughStock()
+            {
+            //Arrange
+            const string TEST_PRODUCT_ID = "1";
+
+            var users = new List<User>();
+            users.Add(createTestUser("Test User", "", 100.00));
+
+            var products = new List<Product>();
+            products.Add(createTestProduct(TEST_PRODUCT_ID, "Product", 1.01, 10));
+
+            var dataManager = new DataManager(users, products);
+            var store = new Store(users[0], dataManager);
+
+            //Act
+            store.Purchase(TEST_PRODUCT_ID, 11);
+
+            //Assert
+            Assert.Pass("No assertion really necessary here");
+            }
+
         // THE BELOW CODE IS REQUIRED TO PREVENT THE TESTS FROM MODIFYING THE USERS/PRODUCTS ON FILE
         //  This is not a good unit testing pattern - the unit test dependency on the file system should
         //  actually be broken ... training on how to do this will be coming.
