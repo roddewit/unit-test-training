be085bef641591af6e83318555cd84e13412933b
thePantz <manwithnoname6@hotmail.com>
2016-04-29 15:03:53 -0500

    Exercise 3

--- UnitTestProject/StoreTests.cs
+++ UnitTestProject/StoreTests.cs
@@ -131,6 +131,26 @@ namespace UnitTestProject
             }                
         }
 
+        [Test]
+        [ExpectedException(typeof(OutOfStockException))]
+        public void Test_PurchaseThrowsExceptionWhenStoreIsOutOfStock()
+        {
+            //Arrange
+            const string TEST_PRODUCT_ID = "5";
+
+            var users = new List<User>();
+            users.Add(createTestUser("Keith Tremorin", "Test", 5.00));
+
+            var products = new List<Product>();
+            products.Add(createTestProduct(TEST_PRODUCT_ID, "Product", 1.00, 1));
+
+            var dataManager = new DataManager(users, products);
+            var store = new Store(users[0], dataManager);
+
+            //Act
+            store.Purchase(TEST_PRODUCT_ID, 2);
+        }
+
 
 
         // THE BELOW CODE IS REQUIRED TO PREVENT THE TESTS FROM MODIFYING THE USERS/PRODUCTS ON FILE
